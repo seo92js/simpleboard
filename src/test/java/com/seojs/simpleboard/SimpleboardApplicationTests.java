5dd60963e9cb748561e97cab884474a970e0cc91
Seo JeongSeop <seo92js@gmail.com>
2023-04-09 17:06:28 +0900

    Initial commit

---
+++ src/test/java/com/seojs/simpleboard/SimpleboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seojs.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
