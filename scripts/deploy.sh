9b8ebc9e684f7309887fa348076841120179684d
Seo JeongSeop <seo92js@gmail.com>
2023-04-27 13:09:16 +0900

    배포 자동화 구성, plain.jar 제외 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl simple-board | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl simple-board | grep jar | grep -v plain | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -23,7 +23,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls - tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls - tr $REPOSITORY/*.jar | grep -v plain | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
